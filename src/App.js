4414a2a0d7f31c3b36f14592f23101e111ac1f08
Deepanshu Malviya <mdeepanshu143@gmail.com>
2023-12-02 11:31:31 +0530

    Client and swiper

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import HeroSection from "./components/HeroSection";
 import IntroPanel from "./components/IntroPanel";
 import WhyChooseUs from "./components/WhyChooseUs";
 import GridArea from "./components/GridArea";
+import Clients from "./components/Clients";
+import TeamPanel from "./components/TeamPanel";
 
 function App() {
   return (
-    <Box className="App" sx={{ backgroundColor: "page.main" }}>
+    <Box sx={{ backgroundColor: "page.main" }}>
       <Box sx={{ backgroundColor: "page.secondary" }} mb={4}>
         <Header />
         <HeroSection />
@@ -16,7 +18,9 @@ function App() {
       <Stack spacing={4} mb={4} mx={3}>
         <IntroPanel />
         <WhyChooseUs />
-       <GridArea />
+        <GridArea />
+        <Clients />
+        <TeamPanel />
       </Stack>
     </Box>
   );
